cc1891f610b96be0d41a6dad4a6efbe73c73251d
saleh <saleh.nazih.dev@gmail.com>
2019-11-24 14:52:26 -0300

    Create Welcome GUI

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import Screen, ScreenManager
 
-class MyApp(App):
+class Manager(ScreenManager):
+    pass
+
+class Menu(Screen):
+    pass
+
+
+# class OperationScreen():
+#     def __init__(self):
+#         self.ingnition = ingnition
+
+#     def changeIgnition(self):
+#         return !self.ingnition
+
+class MyApp(App): 
     
-    def build (self):
-        return Button (text='Hello World')
+    def build(self):
+        return Menu()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     MyApp().run()
